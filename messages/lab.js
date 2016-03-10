291aebd21816d21baf730e18e0cf2784aaca626b
ldonov02 <laura.donovan@tufts.edu>
2016-03-10 18:50:01 -0500

    updated parse function in lab.js

--- messages/lab.js
+++ messages/lab.js
@@ -1,5 +1,12 @@
-request = new XMLHttpRequest();
-request.open('get', 'data.json', true);
-//raw = request.responseText;
-messageData = JSON.parse(request.responseText);
-request.send(messageData);
+
+function parse(){
+	request = new XMLHttpRequest();
+	request.open('get', 'data.json', true);
+	request.onReadyStateChange(function(){
+		if (request.state == 4 && request.responseStatus == 200){
+			text = request.responseText;
+			messageText = JSON.parse(text);
+		}
+	})
+	request.send(null);
+}

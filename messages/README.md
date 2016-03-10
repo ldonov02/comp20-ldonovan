43a8a21feb15c56fc378b4dcce7e4c96bd594ad8
ldonov02 <laura.donovan@tufts.edu>
2016-03-10 16:17:11 -0500

    added readme, started working on lab.js

---
+++ messages/README.md
@@ -0,0 +1,14 @@
+# messages
+
+Modified by: Laura Donovan
+
+On: 3/10/16
+
+Implemented correctly: Haven't touched the HTML
+
+Implemented incorrectly: Everything else
+
+
+Collaborators: Tom Rind
+
+Time spent: .5 hour
--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,5 @@
-// Your JavaScript goes here...
+request = new XMLHttpRequest();
+request.open('get', 'data.json', true);
+//raw = request.responseText;
+messageData = JSON.parse(request.responseText);
+request.send(messageData);

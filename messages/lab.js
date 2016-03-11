bab0151227ae47a366425dda4f73b1e6bf01b081
ldonov02 <laura.donovan@tufts.edu>
2016-03-10 20:09:10 -0500

    added to style.css and fixed lab.js

--- messages/lab.js
+++ messages/lab.js
@@ -2,11 +2,23 @@
 function parse(){
 	request = new XMLHttpRequest();
 	request.open('get', 'data.json', true);
-	request.onReadyStateChange(function(){
-		if (request.state == 4 && request.responseStatus == 200){
-			text = request.responseText;
-			messageText = JSON.parse(text);
-		}
-	})
+	request.onreadystatechange = parseMessage;
 	request.send(null);
+
+	function parseMessage(){
+		if (request.readyState == 4 && request.status == 200){
+			text = request.responseText;
+		//	console.log(text);
+			messageText = JSON.parse(text);
+			elem = document.getElementById("messages");
+			for (i = 0; i < messageText.length; i++){
+				console.log(Object.keys(messageText[i]));
+				elem.innerHTML += "<p><span id='text'>" + messageText[i]["content"] + "  " + messageText[i]["username"] + "</span></p>";
+
+			}
+		//	console.log(messageText);
+			
+			//message.innerHTML = JSON.parse(request.responseText);
+		}
+	}
 }

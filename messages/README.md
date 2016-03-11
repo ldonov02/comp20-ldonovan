9716cd7ee41c67d66adbb50a91316ebf7131e8e3
ldonov02 <laura.donovan@tufts.edu>
2016-03-10 20:43:39 -0500

    answered questions in readme, removed console outputs in lab.js

--- messages/README.md
+++ messages/README.md
@@ -4,11 +4,17 @@ Modified by: Laura Donovan
 
 On: 3/10/16
 
-Implemented correctly: Haven't touched the HTML
+Implemented correctly: Haven't touched the HTML, JSON parses into XML and is put on the page, JavaScript array elements are handled, used XMLHttpRequest
 
-Implemented incorrectly: Everything else
+Implemented incorrectly: Not super nice-looking
 
 
 Collaborators: Tom Rind
 
-Time spent: .5 hour
+Time spent: 2 hours
+
+Part2: It worked in Firefox but not in IE or Chrome. I feel like it should not work, or rather that it depends on the browser. 
+
+Part3: It does not work; the messages no longer show up even in Firefox. 
+
+Bonus question:  No, because browsers implement same-origin policy. As a result, no domain can read data from another domain (be it your local computer or another website) by using XMLHttpRequest (unless cross-origin access is allowed or some other, non-default workaround is implemented).
--- messages/lab.js
+++ messages/lab.js
@@ -1,24 +1,23 @@
 
 function parse(){
+
 	request = new XMLHttpRequest();
+	
 	request.open('get', 'data.json', true);
 	request.onreadystatechange = parseMessage;
 	request.send(null);
 
 	function parseMessage(){
 		if (request.readyState == 4 && request.status == 200){
+
 			text = request.responseText;
-		//	console.log(text);
 			messageText = JSON.parse(text);
 			elem = document.getElementById("messages");
-			for (i = 0; i < messageText.length; i++){
-				console.log(Object.keys(messageText[i]));
-				elem.innerHTML += "<p><span id='text'>" + messageText[i]["content"] + "  " + messageText[i]["username"] + "</span></p>";
 
+			for (i = 0; i < messageText.length; i++){
+				elem.innerHTML += "<p><span id='text'>" + messageText[i]["content"] 
+				+ "  " + messageText[i]["username"] + "</span></p>";
 			}
-		//	console.log(messageText);
-			
-			//message.innerHTML = JSON.parse(request.responseText);
 		}
 	}
 }
